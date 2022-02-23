0b65155bd4668c78290dfd3b31c7756062aa4038
Kimiko Escovilla <78675012+kimikoesc@users.noreply.github.com>
2022-02-23 20:18:02 +0900

    Time: 81 ms, Memory: 46.8 MB - LeetHub

--- valid-palindrome/valid-palindrome.js
+++ valid-palindrome/valid-palindrome.js
@@ -6,6 +6,5 @@ var isPalindrome = function(s) {
     const sCopy = s.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/\s/g, "");
     const sCopyReverse = sCopy.split('').reverse().join('');
     
-    console.log(sCopy, sCopyReverse)
     return sCopy === sCopyReverse;
 };
